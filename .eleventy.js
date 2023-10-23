6dc5f752bc180bb308f08b384980ce2dc2c6d0a6
inc0der <inc0der.dev@gmail.com>
2023-10-23 16:10:39 -0300

    add shortcode for message box

--- .eleventy.js
+++ .eleventy.js
@@ -3,14 +3,31 @@ const markdownIt = require('markdown-it')
 const markdownItAnchor = require('markdown-it-anchor')
 const pluginTOC = require('./plugins/plugin-toc/.eleventy.js')
 
-module.exports = function(config) {
-  config.setLibrary('md', markdownIt().use(markdownItAnchor))
+module.exports = function (config) {
+  const mdOptions = {
+    html: true
+  }
+  config.setLibrary('md', markdownIt(mdOptions).use(markdownItAnchor))
   config.addPlugin(pluginTOC, {
     ul: true,
     wrapper: 'aside',
     wrapperClass: 'menu',
     tags: ['h1', 'h2', 'h3', 'h4']
   })
+  
+  config.addPairedShortcode('message', function (content, label = 'Note', type = 'is-dark') {
+    return `
+      <article class="message ${type}">
+        <div class="message-header">
+          <p>${label}</p>
+        </div>
+        <div class="message-body">
+          ${content}
+        </div>
+      </article>
+    `;
+  });
+
   config.addPassthroughCopy('src/assets/')
   return {
     dir: {
