433b60b8c7f8f1f11817c442a7b6336bef78f492
inc0der <inc0der.dev@gmail.com>
2023-10-22 21:50:13 -0300

    add toc plugin with customizations

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
 const siteData = require('./src/_data/config');
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+const pluginTOC = require('./plugin-toc/.eleventy.js')
 
 module.exports = function(config) {
+  config.setLibrary('md', markdownIt().use(markdownItAnchor))
+  config.addPlugin(pluginTOC, {
+    ul: true,
+    wrapper: 'aside',
+    wrapperClass: 'menu',
+    tags: ['h1', 'h2', 'h3', 'h4']
+  })
   config.addPassthroughCopy('src/assets/')
   return {
     dir: {
